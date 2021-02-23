f762df9c60720c41541c011cdebc8a7ad8b845c0
Gabriel Davi <gdavi.gd@gmail.com>
2021-02-23 15:35:09 -0300

    finish integration

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const axios = require("axios");
+const url = require("url");
+const { query } = require("express");
 require("dotenv/config");
 
 const app = express();
@@ -29,7 +31,7 @@ app.use("/test", async (req, res) => {
     },
   });
 
-  const access_token = response.data.access_token;
+  const { access_token, app_id } = response.data;
 
   response = await axios.get("https://slack.com/api/conversations.list", {
     params: {
@@ -40,6 +42,32 @@ app.use("/test", async (req, res) => {
   const channel_id = response.data.channels[0].id;
 
   response = await axios.post(
+    "https://slack.com/api/conversations.invite",
+    {
+      channel: channel_id,
+      users: app_id,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  );
+
+  return res.redirect(
+    url.format({
+      pathname: "/send-message",
+      query: {
+        channel_id,
+        access_token,
+      },
+    })
+  );
+});
+
+app.use("/send-message", async (req, res) => {
+  const { channel_id, access_token } = req.query;
+  const response = await axios.post(
     "https://slack.com/api/chat.postMessage",
     {
       channel: channel_id,
@@ -51,15 +79,12 @@ app.use("/test", async (req, res) => {
       },
     }
   );
-
-  console.log(response.data);
-
-  return res.send("teste :)");
+  return res.json(response.data);
 });
 
 app.use("/slack", (req, res) => {
   return res.send(`
-  <a href="https://slack.com/oauth/v2/authorize?client_id=${process.env.CLIENT_ID}&scope=channels:read,chat:write,channels:join&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+  <a href="https://slack.com/oauth/v2/authorize?client_id=${process.env.CLIENT_ID}&scope=channels:read,chat:write,channels:join,channels:manage,groups:write,im:write,mpim:write&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
   `);
 });
 
